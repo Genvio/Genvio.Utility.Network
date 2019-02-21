c841282df272b32412a3137e50fd688d899ba5e8
Antonio Luevano <antonio@genvio.net>
2019-02-21 03:48:09 -0600

    About and prep for a NuGet package

---
+++ Genvio.Utility/about.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genvio.Utility
+{
+    public sealed class About
+    {
+        private static StringBuilder stringBuilder;
+        private static Assembly thisAssembly = Assembly.GetEntryAssembly();
+
+        public static string Version
+        {
+            get
+            {
+                stringBuilder.AppendFormat($"Genvio.Utility Assembly Version: {thisAssembly.GetName().Version.ToString()}\n");
+                stringBuilder.AppendFormat($"Genvio.Utility File Version: {thisAssembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version}\n");
+                stringBuilder.AppendFormat($"Genvio.Utility Assembly Informational Version: {thisAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion}\n");
+
+                return stringBuilder.ToString();
+            }
+        }
+
+        public static string License
+        {
+            get
+            {
+                var resourceStream = thisAssembly.GetManifestResourceStream("Genvio.Utility.resources.license.txt");
+                using (var reader = new StreamReader(resourceStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public static string Info
+        {
+            get
+            {
+                var resourceStream = thisAssembly.GetManifestResourceStream("Genvio.Utility.resources.readme.txt");
+                using (var reader = new StreamReader(resourceStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+
+}
---
+++ Genvio.Utility/resources/readme.txt
@@ -0,0 +1,6 @@
+Genvio.Utility.Network Library
+
+Utility class and custom extension methods, this library is used to perform IP and network tasks in a .net core and .net application
+
+Copyright (c) 2018 Genvio Inc.
+support@genvio.net
